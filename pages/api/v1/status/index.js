13d76b418d1969aace9e61bba0e66aee3eaf251d
pedro dias <phpdias@outlook.com>
2024-02-24 20:30:26 +0000

    .

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,25 @@
 import database from "infra/database.js"
 
 async function status(request, response) {
-    const result = await database.query("SELECT 1 + 1 as sum;");
-    console.log(result.rows);
-    response.status(200).json({ chave: "valor" });
+    // updatedAt
+    const updatedAt = new Date().toISOString();
+
+    // versionPostgres
+    const databaseVersionResult = await database.query("SHOW server_version;");
+    const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+    // MaxConnections
+
+    // UsedConnections
+
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: databaseVersionValue,
+        }
+      }
+    });
   }
   
   export default status;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,36 @@
+import React, { useEffect } from 'react';
+
+function Home() {
+  useEffect(() => {
+    const originalTitle = document.title; // Armazena o título original ao carregar o componente
+
+    // Função para mudar o título quando o usuário sai da aba
+    const onBlur = () => {
+      document.title = "Volta por favor ☹️";
+    };
+
+    // Função para restaurar o título quando o usuário volta para a aba
+    const onFocus = () => {
+      document.title = originalTitle;
+    };
+
+    // Adiciona os event listeners
+    window.addEventListener("blur", onBlur);
+    window.addEventListener("focus", onFocus);
+
+    // Remove os event listeners quando o componente é desmontado
+    return () => {
+      window.removeEventListener("blur", onBlur);
+      window.removeEventListener("focus", onFocus);
+    };
+  }, []); // O array vazio indica que este efeito não depende de nenhuma prop ou estado, então ele roda apenas uma vez após o primeiro render.
+
+  return <h1>careca, je t'aime 🥰</h1>;
+}
+
+export default Home;
+
+
 // let docTitle = document.title;
 // window.addEventListener("blur", () => {
 //  document.title = "Volta por favor ☹️";
@@ -7,8 +40,8 @@
 //  document.title = docTitle;
 // })
 
-function Home() {
-  return <h1> careca, je t'aime 🥰</h1>;
-}
+// function Home() {
+//   return <h1> careca, je t'aime 🥰</h1>;
+// }
 
-export default Home;
+// export default Home;
